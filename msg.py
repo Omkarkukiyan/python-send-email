ab48d1d64b951b6eca85362ed9ea6f4a1ccc2fcc
Omkar <33160289+Omkarkukiyan@users.noreply.github.com>
2019-01-19 02:45:22 +0530

    Added

---
+++ msg.py
@@ -0,0 +1,18 @@
+#he code below sends a plain-text email using SMTP_SSL():
+
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"
+receiver_email = "your@gmail.com"
+password = input("Type your password and press enter: ")
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
---
+++ msg2.py
@@ -0,0 +1,44 @@
+#Adding Attachments Using the email Package
+#The code below shows how to send an email with a PDF file as an attachment:
+
+import email ,smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+subject = "An email send from Sarah"
+body = "This is an email with an attachment from Sarah"
+sender_email = "mathewsarah45@gmail.com"
+receiver_email = "mathewsarah45@gmail.com"
+password = input("Type your password and press enter:")
+
+message = MIMEMultipart()
+message["Subject"] = subject
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Bcc"] = receiver_email  #Recommended for mass emails
+
+message.attach(MIMEText(body, "plain"))
+
+filename = "document.pdf"
+
+with open(filename, "rb") as attachment:
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+encoders.encode_base64(part)
+
+part.add_header(
+    "Content Disposition",
+    f"attachment;filename={filename}"
+)
+
+message.attach(part)
+text = message.as_string()
+
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
